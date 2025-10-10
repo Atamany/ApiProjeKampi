daf1a1fe3637a050ba107e5b99294b79ac9f39ed
Yiğit ATAMAN <yigitataman1@gmail.com>
2025-10-10 22:02:37 +0300

    #11 Api Proje Kampı - General Mapping ve Auto Mapper

---
+++ ApiProjeKampi_WebAPI/DTOs/FeatureDTOs/CreateFeatureDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiProjeKampi_WebAPI.DTOs.FeatureDTOs
+{
+    public class CreateFeatureDTO
+    {
+        public string Title { get; set; }
+        public string Subtitle { get; set; }
+        public string Description { get; set; }
+        public string VideoUrl { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ ApiProjeKampi_WebAPI/Mapping/GeneralMapping.cs
@@ -0,0 +1,23 @@
+﻿using ApiProjeKampi_WebAPI.DTOs.FeatureDTOs;
+using ApiProjeKampi_WebAPI.DTOs.MessageDTOs;
+using ApiProjeKampi_WebAPI.Entities;
+using AutoMapper;
+
+namespace ApiProjeKampi_WebAPI.Mapping
+{
+    public class GeneralMapping:Profile
+    {
+        public GeneralMapping()
+        {
+            CreateMap<Feature,ResultFeatureDTO>().ReverseMap();
+            CreateMap<Feature,CreateFeatureDTO>().ReverseMap();
+            CreateMap<Feature,UpdateFeatureDTO>().ReverseMap();
+            CreateMap<Feature,GetByIdFeatureDTO>().ReverseMap();
+
+            CreateMap<Message, ResultMessageDTO>().ReverseMap();
+            CreateMap<Message, CreateMessageDTO>().ReverseMap();
+            CreateMap<Message, UpdateMessageDTO>().ReverseMap();
+            CreateMap<Message, GetByIdMessageDTO>().ReverseMap();
+        }
+    }
+}
