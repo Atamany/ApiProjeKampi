6c923f485904819fb2ff5730e70c98126b7e64d5
Yiğit ATAMAN <yigitataman1@gmail.com>
2025-10-11 01:04:06 +0300

    #23 Api Proje Kampı - Api Consume Backend Kodları

--- ApiProjeKampi_WebUI/ViewComponents/_ServiceDefaultComponentPartial.cs
+++ ApiProjeKampi_WebUI/ViewComponents/_ServiceDefaultComponentPartial.cs
@@ -1,11 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ApiProjeKampi_WebUI.DTOs.ServiceDTOs;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace ApiProjeKampi_WebUI.ViewComponents
 {
     public class _ServiceDefaultComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _ServiceDefaultComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("http://localhost:7157/api/Services/");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultServiceDTO>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
