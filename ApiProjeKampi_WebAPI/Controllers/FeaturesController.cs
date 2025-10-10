5a53c9afba8b8921bf0bb918811493375af45f94
Yiğit ATAMAN <yigitataman1@gmail.com>
2025-10-10 22:09:18 +0300

    #12 Api Proje Kampı - Feature Maplemeleri

---
+++ ApiProjeKampi_WebAPI/Controllers/FeaturesController.cs
@@ -0,0 +1,58 @@
+﻿using ApiProjeKampi_WebAPI.Context;
+using ApiProjeKampi_WebAPI.DTOs.FeatureDTOs;
+using ApiProjeKampi_WebAPI.Entities;
+using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjeKampi_WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeaturesController : ControllerBase
+    {
+        private readonly IMapper _mapper;
+        private readonly ApiContext _context;
+        public FeaturesController(IMapper mapper, ApiContext context)
+        {
+            _mapper = mapper;
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult FeatureList()
+        {
+            var values = _context.Features.ToList();
+            return Ok(_mapper.Map<List<ResultFeatureDTO>>(values));
+        }
+        [HttpPost]
+        public IActionResult CreateFeature(CreateFeatureDTO createFeatureDTO)
+        {
+            var value = _mapper.Map<Feature>(createFeatureDTO);
+            _context.Features.Add(value);
+            _context.SaveChanges();
+            return Ok("Ekleme işlemi başarılı!");
+        }
+        [HttpDelete]
+        public IActionResult DeleteFeature(int id)
+        {
+            var value = _context.Features.Find(id);
+            _context.Features.Remove(value);
+            _context.SaveChanges();
+            return Ok("Silme işlemi başarılı!");
+        }
+        [HttpPut]
+        public IActionResult UpdateFeature(UpdateFeatureDTO updateFeatureDTO)
+        {
+            var value = _mapper.Map<Feature>(updateFeatureDTO);
+            _context.Features.Update(value);
+            _context.SaveChanges();
+            return Ok("Güncelleme işlemi başarılı!");
+        }
+        [HttpGet("GetFeature")]
+        public IActionResult GetFeature(int id)
+        {
+            var value = _context.Features.Find(id);
+            return Ok(_mapper.Map<GetByIdFeatureDTO>(value));
+        }
+    }
+}
