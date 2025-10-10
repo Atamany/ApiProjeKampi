c2ee2a84697181b615e3e93f9088e22aeb35768c
Yiğit ATAMAN <yigitataman1@gmail.com>
2025-10-10 21:13:44 +0300

    #7 Api Proje Kampı - Kategori Api İşlemlerinin Tamamlanması

--- ApiProjeKampi_WebAPI/Controllers/CategoriesController.cs
+++ ApiProjeKampi_WebAPI/Controllers/CategoriesController.cs
@@ -14,12 +14,39 @@ namespace ApiProjeKampi_WebAPI.Controllers
         {
             _context = context;
         }
+        [HttpGet]
+        public IActionResult CategoryList()
+        {
+            var values = _context.Categories.ToList();
+            return Ok(values);
+        }
         [HttpPost]
         public IActionResult CreateCategory(Category category)
         {
             _context.Categories.Add(category);
             _context.SaveChanges();
             return Ok(category);
+        }
+        [HttpDelete]
+        public IActionResult DeleteCategory(int id)
+        {
+            var value = _context.Categories.Find(id);
+            _context.Categories.Remove(value);
+            _context.SaveChanges();
+            return Ok("Kategori silme işlemi başarılı!");
+        }
+        [HttpGet("GetCategory")]
+        public IActionResult GetCategory(int id)
+        {
+            var value = _context.Categories.Find(id);
+            return Ok(value);
+        }
+        [HttpPut]
+        public IActionResult UpdateCategory(Category category)
+        {
+            _context.Categories.Update(category);
+            _context.SaveChanges();
+            return Ok("Kategori güncelleme işlemi başarılı!");
         }
     }
 }
