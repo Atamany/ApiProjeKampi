2608cac5483648cbca20f683aee8ba710108fa7b
Yiğit ATAMAN <yigitataman1@gmail.com>
2025-10-10 21:05:43 +0300

    #6 Api Proje Kampı - Swagger Aracı, DI ve Kategori Ekleme İşlemi

---
+++ ApiProjeKampi_WebAPI/Controllers/CategoriesController.cs
@@ -0,0 +1,25 @@
+﻿using ApiProjeKampi_WebAPI.Context;
+using ApiProjeKampi_WebAPI.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjeKampi_WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ApiContext _context;
+        public CategoriesController(ApiContext context)
+        {
+            _context = context;
+        }
+        [HttpPost]
+        public IActionResult CreateCategory(Category category)
+        {
+            _context.Categories.Add(category);
+            _context.SaveChanges();
+            return Ok(category);
+        }
+    }
+}
