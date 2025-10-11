d15e562c1beee1df4ed22a71410bfe2c7d5e32b2
Yiğit ATAMAN <yigitataman1@gmail.com>
2025-10-11 19:09:51 +0300

    #29 Api Proje Kampı - Events Api İşlemlerinin Tamamlanması

--- ApiProjeKampi_WebUI/ViewComponents/_FeatureDefaultComponentPartial.cs
+++ ApiProjeKampi_WebUI/ViewComponents/_FeatureDefaultComponentPartial.cs
@@ -1,11 +1,26 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ApiProjeKampi_WebUI.DTOs.FeatureDTOs;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace ApiProjeKampi_WebUI.ViewComponents
 {
     public class _FeatureDefaultComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke()
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _FeatureDefaultComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7157/api/Features/");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultFeatureDTO>>(jsonData);
+                return View(values.FirstOrDefault());
+            }
             return View();
         }
     }
