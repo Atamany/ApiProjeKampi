7ce6d91f210f82c95c7a3a507708aa6adba4002f
Yiğit ATAMAN <yigitataman1@gmail.com>
2025-10-12 00:33:22 +0300

    #43 Api Proje Kampı - Ürün / Kategori İlişkisi Api ve Consume

---
+++ ApiProjeKampi_WebUI/DTOs/TestimonialDTOs/CreateTestimonialDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiProjeKampi_WebUI.DTOs.TestimonialDTOs
+{
+    public class CreateTestimonialDTO
+    {
+        public string NameSurname { get; set; }
+        public string Title { get; set; }
+        public string Comment { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ ApiProjeKampi_WebUI/DTOs/TestimonialDTOs/GetByIdTestimonialDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace ApiProjeKampi_WebUI.DTOs.TestimonialDTOs
+{
+    public class GetByIdTestimonialDTO
+    {
+        public int TestimonialId { get; set; }
+        public string NameSurname { get; set; }
+        public string Title { get; set; }
+        public string Comment { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
