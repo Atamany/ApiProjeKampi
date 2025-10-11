b19f950fbb08b5c5e226745329ff6997badd9cea
Yiğit ATAMAN <yigitataman1@gmail.com>
2025-10-11 17:00:42 +0300

    #26 Api Proje Kampı - Menünün UI'da Listelenmesi

---
+++ ApiProjeKampi_WebUI/ViewComponents/DefaultMenuViewComponents/_DefaultMenuProductViewComponentPartial.cs
@@ -0,0 +1,27 @@
+﻿using ApiProjeKampi_WebUI.DTOs.ProductDTOs;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ApiProjeKampi_WebUI.ViewComponents.DefaultMenuViewComponents
+{
+    public class _DefaultMenuProductViewComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _DefaultMenuProductViewComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7157/api/Products/");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultProductDTO>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
