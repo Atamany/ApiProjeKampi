aef8092bca23add916c3eed383feb92d4dd6b5d7
Yiğit ATAMAN <yigitataman1@gmail.com>
2025-10-12 01:42:49 +0300

    #46 Api Proje Kampı - Why Choose Yummy

---
+++ ApiProjeKampi_WebUI/Controllers/EventController.cs
@@ -0,0 +1,77 @@
+﻿using ApiProjeKampi_WebUI.DTOs.EventDTOs;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ApiProjeKampi_WebUI.Controllers
+{
+    public class EventController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public EventController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> EventList()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7157/api/YummyEvents/");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultEventDTO>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult CreateEvent()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateEvent(CreateEventDTO createEventDTO)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createEventDTO);
+            StringContent stringContent = new StringContent(jsonData, System.Text.Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7157/api/YummyEvents/", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("EventList");
+            }
+            return View();
+        }
+        public async Task<IActionResult> DeleteEvent(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"https://localhost:7157/api/YummyEvents?id={id}");
+            return RedirectToAction("EventList");
+        }
+        [HttpGet]
+        public async Task<IActionResult> UpdateEvent(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7157/api/YummyEvents/GetEvent?id={id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateEventDTO>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateEvent(UpdateEventDTO updateEventDTO)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateEventDTO);
+            StringContent stringContent = new StringContent(jsonData, System.Text.Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:7157/api/YummyEvents/", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("EventList");
+            }
+            return View();
+        }
+    }
+}
