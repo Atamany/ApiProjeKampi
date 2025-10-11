385b0458a29a1fbcdbf102a4c6efa2c0d3991ab2
Yiğit ATAMAN <yigitataman1@gmail.com>
2025-10-11 21:23:15 +0300

    #35 Api Proje Kampı - Navbarda Notification Listesi

---
+++ ApiProjeKampi_WebUI/ViewComponents/AdminLayoutNavbarViewComponents/_NavbarNotificationListAdminLayoutComponentPartial.cs
@@ -0,0 +1,27 @@
+﻿using ApiProjeKampi_WebUI.DTOs.NotificationDTOs;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ApiProjeKampi_WebUI.ViewComponents.AdminLayoutNavbarViewComponents
+{
+    public class _NavbarNotificationListAdminLayoutComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _NavbarNotificationListAdminLayoutComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7157/api/Notifications/NotificationListByIsReadFalse");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultNotificationDTO>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+    }
+}
