032ff3ba231c39720094931cb23e1ca1b8f1c37a
Yiğit ATAMAN <yigitataman1@gmail.com>
2025-10-25 10:05:40 +0300

    #54 Api Proje Kampı - Admin Paneli Mesaj İşlemleri

---
+++ ApiProjeKampi_WebUI/Controllers/MessageController.cs
@@ -0,0 +1,77 @@
+﻿using ApiProjeKampi_WebUI.DTOs.MessageDTOs;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace ApiProjeKampi_WebUI.Controllers
+{
+    public class MessageController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        public MessageController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IActionResult> MessageList()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7157/api/Messages/");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultMessageDTO>>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult CreateMessage()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateMessage(CreateMessageDTO createMessageDTO)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createMessageDTO);
+            StringContent stringContent = new StringContent(jsonData, System.Text.Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("https://localhost:7157/api/Messages/", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("MessageList");
+            }
+            return View();
+        }
+        public async Task<IActionResult> DeleteMessage(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.DeleteAsync($"https://localhost:7157/api/Messages?id={id}");
+            return RedirectToAction("MessageList");
+        }
+        [HttpGet]
+        public async Task<IActionResult> UpdateMessage(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync($"https://localhost:7157/api/Messages/GetMessage?id={id}");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<UpdateMessageDTO>(jsonData);
+                return View(values);
+            }
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateMessage(UpdateMessageDTO updateMessageDTO)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(updateMessageDTO);
+            StringContent stringContent = new StringContent(jsonData, System.Text.Encoding.UTF8, "application/json");
+            var responseMessage = await client.PutAsync("https://localhost:7157/api/Messages/", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("MessageList");
+            }
+            return View();
+        }
+    }
+}
