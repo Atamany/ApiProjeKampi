d7aa36aae066c153ef4daad1b8cc40e31cd304cd
Yiğit ATAMAN <yigitataman1@gmail.com>
2025-12-14 17:00:18 +0300

    #69 Api Proje Kampı - Dashboard

---
+++ ApiProjeKampi_WebUI/ViewComponents/DashboardViewComponents/_DashboardWidgetComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjeKampi_WebUI.ViewComponents.DashboardViewComponents
+{
+    public class _DashboardWidgetComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
