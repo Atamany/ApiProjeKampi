731d552c1442d3ecb846ca22f56fadf3b6aabec8
Yiğit ATAMAN <yigitataman1@gmail.com>
2025-12-15 16:17:43 +0300

    #79 Api Proje Kampı - Ana Sayfada İstatistikler

---
+++ ApiProjeKampi_WebAPI/Controllers/StatisticsController.cs
@@ -0,0 +1,42 @@
+﻿using ApiProjeKampi_WebAPI.Context;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiProjeKampi_WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatisticsController : ControllerBase
+    {
+        private readonly ApiContext _context;
+
+        public StatisticsController(ApiContext context)
+        {
+            _context = context;
+        }
+        [HttpGet("ProductCount")]
+        public IActionResult ProductCount()
+        {
+            var value = _context.Products.Count();
+            return Ok(value);
+        }
+        [HttpGet("ReservationCount")]
+        public IActionResult ReservationCount()
+        {
+            var value = _context.Reservations.Count();
+            return Ok(value);
+        }
+        [HttpGet("CategoryCount")]
+        public IActionResult CategoryCount()
+        {
+            var value = _context.Categories.Count();
+            return Ok(value);
+        }
+        [HttpGet("YummyEventCount")]
+        public IActionResult YummyEventCount()
+        {
+            var value = _context.YummyEvents.Count();
+            return Ok(value);
+        }
+    }
+}
