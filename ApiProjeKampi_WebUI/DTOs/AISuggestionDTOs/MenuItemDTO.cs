364fa826978db0630594da3896461efb1155e54a
Yiğit ATAMAN <yigitataman1@gmail.com>
2025-12-16 19:01:40 +0300

    #91 Api Proje Kampı - Grup Rezervasyonlarında AI Destekli Düzenleme

---
+++ ApiProjeKampi_WebUI/DTOs/AISuggestionDTOs/MenuItemDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace ApiProjeKampi_WebUI.DTOs.AISuggestionDTOs
+{
+    public class MenuItemDTO
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
